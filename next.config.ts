636459332f14ddd5362b257f0a27288ba7326b41
Israt Jahan Jisha <isratjahanfci@gmail.com>
2026-02-28 15:44:33 +0600

    add meals page and single meal page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
+import { env } from "@/env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", //  any https image allow 
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${env.NEXT_PUBLIC_AUTH_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
